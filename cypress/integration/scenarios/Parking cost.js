785f16562c7a8dd702c266e7746ed09c0115f7d1
monica27282 <44579128+monica27282@users.noreply.github.com>
2020-11-19 13:23:41 -0600

    Costos y viewport

--- cypress/integration/scenarios/Parking cost.js
+++ cypress/integration/scenarios/Parking cost.js
@@ -1,16 +1,33 @@
+
 describe('Validation of parking costs', () => {
     beforeEach(function(){
         cy.visit('/');
     })
 
-      it('User doesnt enter any data', ()=>{
+      // Valida que muestra mensaje de error cuando el usuario no ingreso ningún dato
+    it('User doesnt enter any data', ()=>{
         cy.get('[name="Submit"]').click();
-        cy.get('body > b:nth-of-type(1)').should(($anyd) => {
-            expect($anyd.text()).to.include('Warning')
+        cy.get('table b').should(($anyd) => {
+            expect($anyd.text()).to.include('ERROR! Enter A Correctly Formatted Date')
           })
     })
+      //Valida que muestre el error cuando el usuario ingreso en fecha de ingreso una fecha mayor a la fecha de salida
+    it('User enters wrong date', ()=>{
+      cy.get('#ParkingLot').select('Valet Parking');
+      cy.get('[name="StartingDate"]').clear().type('11/19/2020');
+      cy.get('[name="StartingTime"]').clear().type('12:00');
+      cy.get('[name="StartingTimeAMPM"]').check();
+      cy.get('[name="LeavingDate"]').clear().type('11/18/2020');
+      cy.get('[name="LeavingTime"]').clear().type('12:00');
+      cy.get('[name="LeavingTimeAMPM"]').check();
+      cy.get('[name="Submit"]').click();
+      cy.get('b').should(($cost1) => {
+          expect($cost1.text()).to.eq('ERROR! Your Leaving Date Or Time Is Before Your Starting Date or Time')
+        })
+    })
 
-    it('Validando Valet Parking', ()=>{
+      //Valida el costo de un día
+    it('Validate Valet Parking', ()=>{
         cy.get('#ParkingLot').select('Valet Parking');
         cy.get('[name="StartingDate"]').clear().type('11/18/2020');
         cy.get('[name="StartingTime"]').clear().type('12:00');
@@ -25,7 +42,7 @@ describe('Validation of parking costs', () => {
         cy.get('.BodyCopy > b').should(($cost3) => {
             expect($cost3.text()).to.include('(1 Days, 0 Hours, 0 Minutes)')
           })
-
+      // valida el costo por hora
         cy.get('#ParkingLot').select('Valet Parking');
         cy.get('[name="StartingDate"]').clear().type('11/20/2020');
         cy.get('[name="StartingTime"]').clear().type('12:00');
@@ -38,11 +55,12 @@ describe('Validation of parking costs', () => {
             expect($cost1.text()).to.eq('$ 18.00')
            })
         cy.get('.BodyCopy > b').should(($cost3) => {
-            expect($cost3.text()).to.include('(0 Days, 4 Hours, 0 Minutes)')
+            expect($cost3.text()).to.include('(0 Days, 16 Hours, 0 Minutes)')
             })
       })
 
-      it('Validando Short Term Parking', ()=>{
+      // valida el costo por hora en short parking
+      it('Validate Short Term Parking', ()=>{
         cy.get('#ParkingLot').select('Short-Term Parking');
         cy.get('[name="StartingDate"]').clear().type('11/20/2020');
         cy.get('[name="StartingTime"]').clear().type('12:00');
@@ -57,7 +75,7 @@ describe('Validation of parking costs', () => {
         cy.get('.BodyCopy > b').should(($cost3) => {
             expect($cost3.text()).to.include('(0 Days, 2 Hours, 0 Minutes)')
           })
-
+      //valida el máximo costo de un día de parqueo
         cy.get('#ParkingLot').select('Short-Term Parking');
         cy.get('[name="StartingDate"]').clear().type('11/20/2020');
         cy.get('[name="StartingTime"]').clear().type('12:00');
@@ -73,7 +91,8 @@ describe('Validation of parking costs', () => {
 
         })
 
-      it('Validando Long Term Garage Parking', ()=>{
+    //valida el costo en horas del long parking
+      it('Validate Long Term Garage Parking', ()=>{
         cy.get('#ParkingLot').select('Long-Term Garage Parking');
         cy.get('[name="StartingDate"]').clear().type('11/20/2020');
         cy.get('[name="StartingTime"]').clear().type('12:00');
@@ -88,7 +107,7 @@ describe('Validation of parking costs', () => {
         cy.get('.BodyCopy > b').should(($cost3) => {
               expect($cost3.text()).to.include('(0 Days, 6 Hours, 0 Minutes)')
               })
-    
+    // valida el costo de parqueo de una semana
         cy.get('#ParkingLot').select('Long-Term Garage Parking');
         cy.get('[name="StartingDate"]').clear().type('11/19/2020');
         cy.get('[name="StartingTime"]').clear().type('12:00');
@@ -101,10 +120,10 @@ describe('Validation of parking costs', () => {
         cy.get('.BodyCopy > b').should(($cost3) => {
               expect($cost3.text()).to.include('(7 Days, 0 Hours, 0 Minutes)')
               })
-         
     })
 
-    it('Validando Long Term Surface Parking', ()=>{
+    //valida el costo en horas de long surface parking
+    it('Validate Long Term Surface Parking', ()=>{
        cy.get('#ParkingLot').select('Long-Term Surface Parking');
        cy.get('[name="StartingDate"]').clear().type('11/19/2020');
        cy.get('[name="StartingTime"]').clear().type('12:00');
@@ -119,7 +138,7 @@ describe('Validation of parking costs', () => {
        cy.get('.BodyCopy > b').should(($cost3) => {
           expect($cost3.text()).to.include('(0 Days, 4 Hours, 0 Minutes)')
           })
-
+      //valida el costo semanal
         cy.get('#ParkingLot').select('Long-Term Surface Parking');
         cy.get('[name="StartingDate"]').clear().type('11/19/2020');
         cy.get('[name="StartingTime"]').clear().type('12:00');
@@ -135,7 +154,8 @@ describe('Validation of parking costs', () => {
 
         })
 
-    it('Validando Economy Lot Parking', ()=>{
+    // valida el costo en horas del economy parking
+    it('Validate Economy Lot Parking', ()=>{
          cy.get('#ParkingLot').select('Economy Parking');
          cy.get('[name="StartingDate"]').clear().type('11/19/2020');
          cy.get('[name="StartingTime"]').clear().type('12:00');
@@ -148,7 +168,7 @@ describe('Validation of parking costs', () => {
          cy.get('.BodyCopy > b').should(($cost3) => {
                expect($cost3.text()).to.include('(0 Days, 4 Hours, 0 Minutes)')
                })
-     
+     // valida el costo semanal
           cy.get('#ParkingLot').select('Economy Parking');
           cy.get('[name="StartingDate"]').clear().type('11/19/2020');
           cy.get('[name="StartingTime"]').clear().type('12:00');
--- cypress/integration/scenarios/Validation Elements Parking cost.js
+++ cypress/integration/scenarios/Validation Elements Parking cost.js
@@ -1,10 +1,10 @@
 describe('Website elements validation', () => {
-    beforeEach(function(){
-        cy.visit('/');
-    })
+   
     it('Validate items', ()=>{
-
+        cy.visit('/');
+        //Valida que el titulo de la página este visible
         cy.get('.PageTitle').should('be.visible');
+        //Valida que exista el texto correcto
         cy.get('.BodyCopy').contains('Choose a Parking Lot').should('exist');
         cy.get('#ParkingLot').contains('Valet Parking').should('exist');
         cy.get('.BodyCopy').contains('Please input entry date and time').should('exist');
@@ -16,8 +16,27 @@ describe('Website elements validation', () => {
         cy.get('p').contains('$18 per day').should('be.visible');
         cy.get('p').contains('$12 for five hours or less').should('be.visible');
         cy.get('i').contains('A Lost Ticket Fee of $10.00 will be assessed when the original parking stub cannot be produced when exiting the parking facilities (does not apply to Valet Parking).').should('be.visible');
+        //valida que los radio botones AM esten en estado checked
         cy.get('[value="AM"]').should('be.checked');
 
     })
+    //valida cómo se veria en distintas viewport el sitio web
+    it('Validate viewport iphone-x', ()=>{
+
+        cy.viewport('iphone-x')
+        cy.visit('/')
+        cy.wait(3000)
+
+    })
+
+    it('Validate viewport ipad Mini', ()=>{
+
+        cy.viewport('ipad-mini')
+        cy.visit('/')
+        cy.wait(3000)
+
+    })
+
+
 })
 
